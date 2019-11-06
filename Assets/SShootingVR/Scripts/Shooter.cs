59a8b11858f7e7b7ba1372b0dc1645c2f9382265
NJC-T1 <toda7.mk3@gmail.com>
2019-11-06 19:24:59 +0900

    2019-11-06 ２回目のコミット　弾を生成した。まだ飛ばない。

---
+++ Assets/SShootingVR/Scripts/Shooter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab; //弾のプレハブ
+    [SerializeField] Transform gunBarrelEnd; //銃口（弾の発射位置）
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //入力に応じて弾を発射する
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
+
+    }
+
+    void Shoot()
+    {
+        //プレハブを元に、シーン上に弾を生成
+        Instantiate(bulletPrefab, gunBarrelEnd.position, gunBarrelEnd.rotation);
+
+    }
+}
