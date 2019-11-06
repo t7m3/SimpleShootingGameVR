aac7d9ef5dd9b2f93bd9c85a0e20d502a9fa9548
user224 <toda7.mk3@gmail.com>
2019-11-06 17:23:58 +0900

    2019-11-06(水) 1回目のコミット　地面とプレイヤーを配置し、矢印キーでカメラが回転するようにした。

---
+++ Assets/SShootingVR/Scripts/CameraRotator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    [SerializeField] float angularVelocity = 30f; //回転速度の設定
+    float horizontalAngle = 0f; //水平方向の回転量を保存
+    float verticalAngle = 0f; //垂直方向の回転量を保存
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //入力による回転量を取得
+        var horizontalRotation = Input.GetAxis("Horizontal") * angularVelocity * Time.deltaTime;
+        var verticalRotation = -Input.GetAxis("Vertical") * angularVelocity * Time.deltaTime;
+
+        //回転量を更新
+        horizontalAngle += horizontalRotation;
+        verticalAngle += verticalRotation;
+
+        //垂直方向は回転し過ぎないように制限
+        verticalAngle = Mathf.Clamp(verticalAngle, -80f, 80f);
+
+        //Transformコンポーネントに回転量を適用する
+        transform.rotation = Quaternion.Euler(verticalAngle, horizontalAngle, 0f);
+
+    }
+}
