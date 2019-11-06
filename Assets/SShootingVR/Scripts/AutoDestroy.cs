b7340a1114ed5dd5a27f82f88b59113c9d07d4cc
NJC-T1 <toda7.mk3@gmail.com>
2019-11-06 20:01:09 +0900

    2019-11-06 ３回目のコミット　弾が飛んで、一定時間後に破棄されるようにした。

---
+++ Assets/SShootingVR/Scripts/AutoDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    [SerializeField] float lifetime = 5f; //ゲームオブジェクトの寿命 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //一定時間経過後にゲームオブジェクトを破棄する
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SShootingVR/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 20f; //弾速 [m/s]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ゲームオブジェクト前方向の速度ベクトルを計算
+        var velocity = speed * transform.forward;
+
+        //Rigidbodyコンポーネントを取得
+        var rigidbody = GetComponent<Rigidbody>();
+
+        //Rigidbodyコンポーネントを使って初速を与える
+        rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
