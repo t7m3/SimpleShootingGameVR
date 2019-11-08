9bbae1505d2ffb0be0c06016420dae2feb458f50
NJC-T1 <toda7.mk3@gmail.com>
2019-11-09 00:48:53 +0900

    2019-11-08(金) １回目のコミット　VR不調（発射位置が動かない）。銃口位置とカメラ位置をＵＩ(Text)に表示するようにした。

--- Assets/SShootingVR/Scripts/Shooter.cs
+++ Assets/SShootingVR/Scripts/Shooter.cs
@@ -2,30 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//追加
+using UnityEngine.VR;
+
+//　デバグ用
+using UnityEngine.UI;
+
+
+//　デバグ用
+//[RequireComponent(typeof(Text))]
+
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab; //弾のプレハブ
     [SerializeField] Transform gunBarrelEnd; //銃口（弾の発射位置）
 
+
+    //　デバグ用
+    [SerializeField] Text uiText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //　デバグ用
+        uiText.text = "はじまり";
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        Vector3 p1 = gunBarrelEnd.position;
+        uiText.text = string.Format("ここでは x:{0:F}, y:{1:F}, z:{2:F}", p1.x, p1.y, p1.z);
+
         //入力に応じて弾を発射する
         if (Input.GetButtonDown("Fire1"))
+        {
             Shoot();
+        }
 
     }
 
     void Shoot()
     {
+
+        Camera cam = Camera.main;
+
+        //Vector3 p1 = new Vector3(0.0f,      // x
+        //                         0.0f,      // y
+        //                         0.0f);     // z    
+
+        Vector3 p1 = gunBarrelEnd.position;    
+        Quaternion r1 = gunBarrelEnd.rotation;
+        //Quaternion r1 = cam.transform.rotation;
+
+        //　デバグ用
+        uiText.text = "shoot関数に来た";
+        uiText.text = string.Format("ここでは x:{0:F}, y:{1:F}, z:{2:F}", p1.x, p1.y, p1.z);
+        Debug.Log("x = " + p1.x + "、y = " + p1.y + "、z = " + p1.z);
+
         //プレハブを元に、シーン上に弾を生成
-        Instantiate(bulletPrefab, gunBarrelEnd.position, gunBarrelEnd.rotation);
+        //Instantiate(bulletPrefab, gunBarrelEnd.position, gunBarrelEnd.rotation);
+
+        Instantiate(bulletPrefab, p1, r1);
 
     }
 }
---
+++ Assets/SShootingVR/Scripts/cameraPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//　デバグ用
+using UnityEngine.UI;
+
+public class cameraPosition : MonoBehaviour
+{
+
+    //　デバグ用
+    [SerializeField] Text uiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 p1 = transform.position;
+        Quaternion r1 = transform.rotation;
+
+        //uiText.text = string.Format("ここでは x:{0:F}, y:{1:F}, z:{2:F}", p1.x, p1.y, p1.z);
+        uiText.text = string.Format("ここでは x:{0:F}, y:{1:F}, z:{2:F}", r1.x, r1.y, r1.z);
+
+    }
+}
